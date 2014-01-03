05e85355aa4a5a23ac0a933d69ec82fe8b8efce5
Wallace Sidhrée <i@dreamyguy.com>
2014-01-03 16:16:13 +0100

    Introduce app

--- app/js/app.js
+++ app/js/app.js
@@ -4,13 +4,54 @@ function hw() {
 }
 
 // the app!
-var myAppModule = angular.module('myApp', []);
+var myAppModule = angular.module('project', ['ngRoute', 'firebase'])
 
-myAppModule.controller('myController', function($scope, $window) {
+.value('fbURL', 'https://angularjs-projects.firebaseio.com')
 
-    // do something
+.factory('Projects', function($firebase, fbURL) {
+    return $firebase(new Firebase(fbURL));
+});
 
-    // render showOnLoad on page load
-    $scope.showOnLoad = true;
+myAppModule.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        controller:'ListCtrl',
+        templateUrl:'list.html'
+    })
+    .when('/edit/:projectId', {
+        controller:'EditCtrl',
+        templateUrl:'detail.html'
+    })
+    .when('/new', {
+        controller:'CreateCtrl',
+        templateUrl:'detail.html'
+    })
+    .otherwise({
+        redirectTo:'/'
+    });
+});
 
+myAppModule.controller('ListCtrl', function($scope, Projects) {
+    $scope.projects = Projects;
+});
+
+myAppModule.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
+    $scope.save = function() {
+        Projects.$add($scope.project, function() {
+            $timeout(function() { $location.path('/'); });
+        });
+    };
+});
+
+myAppModule.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, fbURL) {
+    var projectUrl = fbURL + $routeParams.projectId;
+    $scope.project = $firebase(new Firebase(projectUrl));
+    $scope.destroy = function() {
+        $scope.project.$remove();
+        $location.path('/');
+    };
+    $scope.save = function() {
+        $scope.project.$save();
+        $location.path('/');
+    };
 });
