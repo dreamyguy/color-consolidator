e5a3209d8b2c6f01e5a640dcb23e31f43c6828e3
Jake Johnson <jjohnson@stempremier.com>
2014-01-05 02:13:03 -0500

    missing trailing forward slash from firebase url

--- app/js/app.js
+++ app/js/app.js
@@ -1,56 +1,72 @@
+// jasmine's first test
+function hw() {
+    return 'Hello World';
+}
+
+// check connection status to Firebase
+function firebaseConn() {
+    var firebaseRef = new Firebase('https://color-consolidator.firebaseio.com/');
+    firebaseRef.child('.info/connected').on('value', function(connectedSnap) {
+        if (connectedSnap.val() === true) {
+            console.log("we're connected to Firebase!");
+        } else {
+            console.log("we're disconnected from Firebase!");
+        }
+    });
+}
+
 // the app!
-angular.module('project', ['ngRoute', 'firebase'])
- 
-.value('fbURL', 'https://angularjs-projects.firebaseio.com/')
- 
+var myAppModule = angular.module('project', ['ngRoute', 'firebase'])
+
+.value('fbURL', 'https://color-consolidator.firebaseio.com/')
+
 .factory('Projects', function($firebase, fbURL) {
-  return $firebase(new Firebase(fbURL));
-})
- 
-.config(function($routeProvider) {
-  $routeProvider
+    return $firebase(new Firebase(fbURL));
+});
+
+myAppModule.config(function($routeProvider) {
+    $routeProvider
     .when('/', {
-      controller:'ListCtrl',
-      templateUrl:'list.html'
+        controller:'ListCtrl',
+        templateUrl:'list.html'
     })
     .when('/edit/:projectId', {
-      controller:'EditCtrl',
-      templateUrl:'detail.html'
+        controller:'EditCtrl',
+        templateUrl:'detail.html'
     })
     .when('/new', {
-      controller:'CreateCtrl',
-      templateUrl:'detail.html'
+        controller:'CreateCtrl',
+        templateUrl:'detail.html'
     })
     .otherwise({
-      redirectTo:'/'
+        redirectTo:'/'
     });
-})
- 
-.controller('ListCtrl', function($scope, Projects) {
-  $scope.projects = Projects;
-})
+});
 
-.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
-  $scope.save = function() {
-    Projects.$add($scope.project, function() {
-      $timeout(function() { $location.path('/'); });
-    });
-  };
-})
- 
-.controller('EditCtrl',
-  function($scope, $location, $routeParams, $firebase, fbURL) {
-    var projectUrl = fbURL + $routeParams.projectId;
-    $scope.project = $firebase(new Firebase(projectUrl));
- 
-    $scope.destroy = function() {
-      $scope.project.$remove();
-      $location.path('/');
-    };
- 
+myAppModule.controller('ListCtrl', function($scope, Projects) {
+    firebaseConn();
+    $scope.projects = Projects;
+});
+
+myAppModule.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
+    firebaseConn();
     $scope.save = function() {
-      $scope.project.$save();
-      $location.path('/');
+        Projects.$add($scope.project, function() {
+            $timeout(function() { $location.path('/'); });
+        });
     };
 });
 
+myAppModule.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, fbURL) {
+    firebaseConn();
+    var projectUrl = fbURL + $routeParams.projectId;
+    $scope.project = $firebase(new Firebase(projectUrl));
+    $scope.destroy = function() {
+        $scope.project.$remove();
+        $location.path('/');
+    };
+    $scope.save = function() {
+        $scope.project.$save();
+        $location.path('/');
+    };
+});
